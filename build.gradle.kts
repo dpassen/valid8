1e82d921e74c9d2978fe71ffcefa2e7f0d5cdf6e
Derek Passen <dpassen1@gmail.com>
2023-09-23 12:25:49 -0500

    Bump lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 dependencies {
     api("com.jnape.palatable:lambda:5.5.0")
 
-    compileOnly("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
